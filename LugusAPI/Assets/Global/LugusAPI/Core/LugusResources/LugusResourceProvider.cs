f9d3e4991e63dfa3d1b7b061940d91c18fc77fc6
Robin Marx <robin@lugus-studios.be>
2013-11-01 19:06:20 +0100

    - Added preliminary text support to LugusResources

--- LugusAPI/Assets/Global/LugusAPI/Core/LugusResources/LugusResourceProvider.cs
+++ LugusAPI/Assets/Global/LugusAPI/Core/LugusResources/LugusResourceProvider.cs
@@ -18,43 +18,11 @@ public interface ILugusResourceProvider
 	
 	Texture2D GetTexture(string key);
 	Texture2D GetTexture(string BaseURL, string key);
+	
 	AudioClip GetAudio(string key);
 	AudioClip GetAudio(string BaseURL, string key);
+	
 	TextAsset GetText(string key);
+	TextAsset GetText(string BaseURL, string key);
 }
 
-public class LugusResourceProviderDisk : MonoBehaviour, ILugusResourceProvider
-{
-	[SerializeField]
-	protected string _baseURL = "";
-	public string BaseURL
-	{
-		get{ return _baseURL; }
-		set{ _baseURL = value; }
-	}
-	
-	public Texture2D GetTexture(string key)
-	{
-		return GetTexture (this.BaseURL, key);
-	}
-	
-	public Texture2D GetTexture(string BaseURL, string key)
-	{
-		return Resources.Load( BaseURL + "Textures/" + key, typeof(Texture2D) ) as Texture2D;
-	}
-	
-	public AudioClip GetAudio(string key)
-	{
-		return GetAudio (this.BaseURL, key);
-	}
-	
-	public AudioClip GetAudio(string BaseURL, string key)
-	{
-		return Resources.Load( BaseURL + "Audio/" + key, typeof(AudioClip) ) as AudioClip;
-	}
-	
-	public TextAsset GetText(string key)
-	{
-		return null;
-	}
-}
--- LugusAPI/Assets/Global/LugusAPI/Core/LugusResources/LugusResourcesDefault.cs
+++ LugusAPI/Assets/Global/LugusAPI/Core/LugusResources/LugusResourcesDefault.cs
@@ -53,6 +53,7 @@ public class LugusResourcesDefault : MonoBehaviour, ILugusResources
 	public List<ILugusResourceProvider> providers = null;
 	
 	protected LugusResourceProviderDisk diskProvider = null;
+	protected LugusResourceHelperText textHelper = null;
 	
 	protected string sharedBaseURL = "Shared/";
 	protected string languagesBaseURL = "Languages/";
@@ -65,23 +66,37 @@ public class LugusResourcesDefault : MonoBehaviour, ILugusResources
 	}
 	
 	protected void FindReferences()
-	{
-		this.diskProvider = gameObject.AddComponent<LugusResourceProviderDisk>(); 
+	{ 
+		if( this.diskProvider == null )
+		{
+			this.diskProvider = gameObject.AddComponent<LugusResourceProviderDisk>(); 
+		}
 		
-		providers = new List<ILugusResourceProvider>();
-		providers.Add( diskProvider );
+		if( this.textHelper == null )
+		{
+			this.textHelper = new LugusResourceHelperText();
+		}
 		
-		SetBaseLanguage( languagesBaseLanguage );
+		if( this.providers == null || this.providers.Count == 0 )
+		{
+			providers = new List<ILugusResourceProvider>();
+			providers.Add( diskProvider );
+		}
 	}
 	
 	public void SetBaseLanguage(string langID)
 	{
+		FindReferences();
+		
 		languagesBaseLanguage = langID;
 		
 		foreach( ILugusResourceProvider provider in providers )
 		{
 			provider.BaseURL = languagesBaseURL + languagesBaseLanguage + "/";
 		}
+		
+		// TODO: now we're loading all text files from disk. If this is no longer the case, we should update this part
+		textHelper.Parse( diskProvider.GetText("texts") );
 		
 		if( onResourcesReloaded != null )
 		{
@@ -94,6 +109,7 @@ public class LugusResourcesDefault : MonoBehaviour, ILugusResources
 	public void Awake()
 	{
 		FindReferences();
+		SetBaseLanguage( languagesBaseLanguage );
 	}
 	
 	public Texture2D GetTexture(string key)
@@ -139,7 +155,7 @@ public class LugusResourcesDefault : MonoBehaviour, ILugusResources
 	
 	public string GetText(string key)
 	{
-		return null;
+		return textHelper.Get ( key );
 	}
 
 }
